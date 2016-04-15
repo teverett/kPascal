61ceda6d0a4a6d16dffe05d3e5917a55b7ee952e
Tom Everett <tom@khubla.com>
2016-04-16 01:36:38 +0200

    cleanup

--- src/main/java/com/khubla/kpascal/interpreter/Context.java
+++ src/main/java/com/khubla/kpascal/interpreter/Context.java
@@ -27,7 +27,7 @@ import com.khubla.kpascal.value.Value;
  * The context is the execution context for the entire program. It contains all global state
  * information about the program including the stack of execution scopes, all global constants and
  * every procedure
- * 
+ *
  * @author teverett
  */
 public class Context {
