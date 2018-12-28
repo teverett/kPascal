5792094ce8dc601db80e0fff4f1d6d688fe75e21
Tom Everett <tom@khubla.com>
2018-12-27 21:00:09 -0700

    spotless

--- src/main/java/com/khubla/kpascal/value/FileValue.java
+++ src/main/java/com/khubla/kpascal/value/FileValue.java
@@ -1,7 +1,3 @@
-package com.khubla.kpascal.value;
-
-import com.khubla.kpascal.exception.InvalidOperationException;
-import com.khubla.kpascal.exception.NotImplementedException;
 /*
 * kPascal Copyright 2015, khubla.com
 *
@@ -18,6 +14,10 @@ import com.khubla.kpascal.exception.NotImplementedException;
 *    You should have received a copy of the GNU General Public License
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+package com.khubla.kpascal.value;
+
+import com.khubla.kpascal.exception.InvalidOperationException;
+import com.khubla.kpascal.exception.NotImplementedException;
 import com.khubla.kpascal.type.FileType;
 import com.khubla.kpascal.type.Type;
 
